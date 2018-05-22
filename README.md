6917528823b393190ecd15ce85bf1daa098748dd
宇野嵐 <araera48@gmail.com>
2018-05-22 10:51:24 +0900

    fix webpack config

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # ObjectiveCanvas
 
 HTML5 Canvasを使いやすくするためのライブラリ。
+
+## Install
+
+`objective_canvas.min.js`をダウンロード後、Scriptタグで読み込んで下さい。
+
+```:zsh
+curl https://raw.githubusercontent.com/sobreera/objective-canvas/master/dist/objective_canvas.min.js -o objective_canvas.min.js
+```
+
+```:html5
+<script src="./js/objective_canvas.min.js"></script>
+```
+
+## Build
+
+```:zsh
+git clone https://github.com/sobreera/objective-canvas.git
+cd objective-canvas
+yarn install
+yarn build
+```
+
+## 対応予定
+
+- オーバーレイオブジェクトの実装
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,22 @@
-module.exports = {
-  entry: {
-    objective_canvas: './src/index.js'
-  },
-  output: {
-    filename: '[name].js',
-    path: __dirname + '/dist',
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclued: /node_modules/
-      }
-    ]
+module.exports = (env, argv) => {
+  return {
+    entry: {
+      objective_canvas: './src/index.js'
+    },
+    output: {
+      filename: argv.mode === 'production' ? '[name].min.js' :'[name].js',
+      path: __dirname + '/dist',
+      library: "obc",
+      libraryTarget: "umd"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: 'babel-loader',
+          exclude: /node_modules/
+        }
+      ]
+    }
   }
 }
